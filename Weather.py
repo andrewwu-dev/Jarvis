e2d2123c48300a1313110ce1657c339bbf969df1
billwu99 <billwu95@gmail.com>
2019-05-17 13:04:43 -0400

    made intentMap

---
+++ Weather.py
@@ -0,0 +1,9 @@
+#weather backend
+
+class Weather:
+    def __init__(self):
+        self.appKey = ""
+        print('initialized Weather')
+
+    def getWeather(self, lat, long):
+        return ""
