d9babddafe950ba54c0b99a13b4c48c82b8fe7b3
Andrew <awu.dev@gmail.com>
2019-05-20 20:38:13 -0400

    Added weather api calling

--- Weather.py
+++ Weather.py
@@ -1,9 +1,45 @@
 #weather backend
+import geocoder
+import requests
+import pprint
 
 class Weather:
     def __init__(self):
-        self.appKey = ""
+        keyFile = open('weatherKey.txt', 'r')
+        if keyFile.mode == 'r':
+            self.key = keyFile.read()
+        assert self.key
+
+        self.url = "http://api.openweathermap.org/data/2.5/weather"
+        self.ip = geocoder.ip('me')
+        
+        self.params = {
+            "lat": self.ip.lat,
+            "lon": self.ip.lng,
+            "units": "metric",
+            "APPID": self.key
+        }
+        
         print('initialized Weather')
 
-    def getWeather(self, lat, long):
-        return ""
+    def getWeather(self):
+        req = requests.get(url=self.url, params=self.params)
+        pp = pprint.PrettyPrinter(indent=4)
+        #pp.pprint(req.json())
+        data = req.json()
+        #print("-------------")
+
+        weatherInfo = {
+            "weather": data["weather"][0]["description"],
+            "temperature": round(data["main"]["temp"])
+        }
+
+        #pp.pprint(weatherInfo)
+
+        return weatherInfo
+
+if __name__ == "__main__":
+    w = Weather()
+
+    w.getWeather()
+
