ccd87abc9f3f740717774713f0ec8612cba4c972
Andrew <awu.dev@gmail.com>
2019-05-25 12:40:46 -0400

    Added text to speech feature

--- Intents.py
+++ Intents.py
@@ -1,21 +1,22 @@
-import Weather, PlayMusic
+import Weather, PlayMusic, TextToSpeech
 
 class Intents:
     def __init__(self): 
         self.weather = Weather.Weather()
-        self.musicPlayer = PlayMusic.PlayMusic()   
+        self.musicPlayer = PlayMusic.PlayMusic()
+        self.speaker = TextToSpeech.TextToSpeech()   
 
     def performAction(self, data):
         intent = data["intent"]
 
         if intent == "PlayMusic":
-
             song = data["entities"].fields["song"].string_value
+            self.speaker.say("Alright, here's " + song)
             self.musicPlayer.play(song)
 
         elif intent == "GetWeather":
             msg = self.weather.getWeather()
-            print (msg)
+            self.speaker.say(msg)
             
         else:
             print (data["response"])
